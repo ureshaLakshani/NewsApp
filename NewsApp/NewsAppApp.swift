76d4ae7ee268cb5ec156521a7a1ef1cddd3be851
ureshaLakshani <ureshalak97@gmail.com>
2022-08-20 18:31:11 +0530

    initial commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Uresha Lakshani on 2022-08-20.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
